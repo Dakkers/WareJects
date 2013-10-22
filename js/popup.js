a952eda5b95d5500d17d84ff34f6ddec8a721b4d
StDako <d.h.stlaurent@gmail.com>
2013-10-22 01:47:39 -0400

    popup window is fixed, added logo

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$('#projects').click(function(){
+		window.open('index.html', "_blank");
+	});
+
+	$("#about").click(function () {
+		window.open('http://www.stdako.com/projects.html', '_blank');
+	});
+});
